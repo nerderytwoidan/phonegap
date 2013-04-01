ac56ed9ed30f615fd3e47c8e77ed556cdde29fd5
Thane Woidan <twoidan@nerdery.com>
2013-04-01 16:03:41 -0500

    Creating some test content, to see how PhoneGap handles viewport sizing.

--- index.php
+++ index.php
@@ -22,6 +22,20 @@
         <script type="text/javascript" src="assets/scripts/global.js"></script>
     </head>
 <body>
-
+    <div class="container container_masthead">
+        <div class="wrap masthead">
+            <h1>PhoneGap Test App</h1>
+        </div>
+    </div>
+    <div class="container container_main">
+        <div class="wrap main">
+            <p>OMG LOL PHONEGAP</p>
+        </div>
+    </div>
+    <div class="container container_main">
+        <div class="wrap footer">
+            <small>Footer legalese</small>
+        </div>
+    </div>
 </body>
 </html>
